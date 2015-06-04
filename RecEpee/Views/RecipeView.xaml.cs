676edc81e82316e9f0200bc16107d6fd0f0834f9
ghembo <invictus86@gmail.com>
2015-06-04 14:51:35 +0200

    Extract recipe details to recipe view

---
+++ RecEpee/Views/RecipeView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RecEpee.Views
+{
+    /// <summary>
+    /// Interaction logic for RecipeView.xaml
+    /// </summary>
+    public partial class RecipeView : DockPanel
+    {
+        public RecipeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
